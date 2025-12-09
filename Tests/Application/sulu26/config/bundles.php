8a4ab997c04df699d87a9ec567c48eb903cc5e0f
Prokyonn <daniel@sulu.io>
2025-12-09 01:41:40 +0100

    Add ArticleBundle to test environment and create templates

--- Tests/Application/sulu26/config/bundles.php
+++ Tests/Application/sulu26/config/bundles.php
@@ -1,102 +1,50 @@
 <?php
 
-/*
- * This file is part of Sulu.
- *
- * (c) Sulu GmbH
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
-
-use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
-use Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle;
-use Doctrine\Bundle\PHPCRBundle\DoctrinePHPCRBundle;
-use FOS\HttpCacheBundle\FOSHttpCacheBundle;
-use FOS\JsRoutingBundle\FOSJsRoutingBundle;
-use FOS\RestBundle\FOSRestBundle;
-use HandcraftedInTheAlps\RestRoutingBundle\RestRoutingBundle;
-use JMS\SerializerBundle\JMSSerializerBundle;
-use Massive\Bundle\BuildBundle\MassiveBuildBundle;
-use Massive\Bundle\SearchBundle\MassiveSearchBundle;
-use PHPCR\PhpcrMigrationsBundle\PhpcrMigrationsBundle;
-use Scheb\TwoFactorBundle\SchebTwoFactorBundle;
-use Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle;
-use Sulu\Bundle\ActivityBundle\SuluActivityBundle;
-use Sulu\Bundle\AdminBundle\SuluAdminBundle;
-use Sulu\Bundle\CategoryBundle\SuluCategoryBundle;
-use Sulu\Bundle\ContactBundle\SuluContactBundle;
-use Sulu\Bundle\CoreBundle\SuluCoreBundle;
-use Sulu\Bundle\CustomUrlBundle\SuluCustomUrlBundle;
-use Sulu\Bundle\DocumentManagerBundle\SuluDocumentManagerBundle;
-use Sulu\Bundle\HashBundle\SuluHashBundle;
-use Sulu\Bundle\HttpCacheBundle\SuluHttpCacheBundle;
-use Sulu\Bundle\LocationBundle\SuluLocationBundle;
-use Sulu\Bundle\MarkupBundle\SuluMarkupBundle;
-use Sulu\Bundle\MediaBundle\SuluMediaBundle;
-use Sulu\Bundle\PageBundle\SuluPageBundle;
-use Sulu\Bundle\PersistenceBundle\SuluPersistenceBundle;
-use Sulu\Bundle\PreviewBundle\SuluPreviewBundle;
-use Sulu\Bundle\ReferenceBundle\SuluReferenceBundle;
-use Sulu\Bundle\RouteBundle\SuluRouteBundle;
-use Sulu\Bundle\SearchBundle\SuluSearchBundle;
-use Sulu\Bundle\SecurityBundle\SuluSecurityBundle;
-use Sulu\Bundle\SnippetBundle\SuluSnippetBundle;
-use Sulu\Bundle\TagBundle\SuluTagBundle;
-use Sulu\Bundle\TestBundle\SuluTestBundle;
-use Sulu\Bundle\TrashBundle\SuluTrashBundle;
-use Sulu\Bundle\WebsiteBundle\SuluWebsiteBundle;
-use Symfony\Bundle\DebugBundle\DebugBundle;
-use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
-use Symfony\Bundle\MonologBundle\MonologBundle;
-use Symfony\Bundle\SecurityBundle\SecurityBundle;
-use Symfony\Bundle\TwigBundle\TwigBundle;
-use Symfony\Bundle\WebProfilerBundle\WebProfilerBundle;
-use Symfony\Cmf\Bundle\RoutingBundle\CmfRoutingBundle;
-
 return [
-    FrameworkBundle::class => ['all' => true],
-    TwigBundle::class => ['all' => true],
-    MonologBundle::class => ['all' => true],
-    SuluCoreBundle::class => ['all' => true],
-    DoctrineBundle::class => ['all' => true],
-    DoctrineFixturesBundle::class => ['all' => true],
-    DoctrinePHPCRBundle::class => ['all' => true],
-    FOSRestBundle::class => ['all' => true],
-    RestRoutingBundle::class => ['all' => true],
-    JMSSerializerBundle::class => ['all' => true],
-    MassiveSearchBundle::class => ['all' => true],
-    FOSHttpCacheBundle::class => ['all' => true],
-    SuluAdminBundle::class => ['all' => true],
-    SuluSearchBundle::class => ['all' => true],
-    SuluPersistenceBundle::class => ['all' => true],
-    SuluContactBundle::class => ['all' => true],
-    SuluMediaBundle::class => ['all' => true],
-    SuluSecurityBundle::class => ['all' => true],
-    SuluCategoryBundle::class => ['all' => true],
-    SuluSnippetBundle::class => ['all' => true],
-    SuluPageBundle::class => ['all' => true],
-    SuluTagBundle::class => ['all' => true],
-    SuluWebsiteBundle::class => ['all' => true],
-    SuluLocationBundle::class => ['all' => true],
-    SuluHttpCacheBundle::class => ['all' => true],
-    SuluDocumentManagerBundle::class => ['all' => true],
-    SuluHashBundle::class => ['all' => true],
-    SuluCustomUrlBundle::class => ['all' => true],
-    SuluRouteBundle::class => ['all' => true],
-    SuluMarkupBundle::class => ['all' => true],
-    PhpcrMigrationsBundle::class => ['all' => true],
-    MassiveBuildBundle::class => ['all' => true],
-    WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    SuluTestBundle::class => ['dev' => true, 'test' => true],
-    DebugBundle::class => ['dev' => true, 'test' => true],
-    SecurityBundle::class => ['all' => true],
-    SuluPreviewBundle::class => ['all' => true],
-    FOSJsRoutingBundle::class => ['all' => true],
-    CmfRoutingBundle::class => ['all' => true, 'website' => true],
-    StofDoctrineExtensionsBundle::class => ['all' => true],
-    SuluActivityBundle::class => ['all' => true],
-    SuluTrashBundle::class => ['all' => true],
-    SuluReferenceBundle::class => ['all' => true],
-    SchebTwoFactorBundle::class => ['all' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Sulu\Bundle\CoreBundle\SuluCoreBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
+    Doctrine\Bundle\PHPCRBundle\DoctrinePHPCRBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    HandcraftedInTheAlps\RestRoutingBundle\RestRoutingBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Massive\Bundle\SearchBundle\MassiveSearchBundle::class => ['all' => true],
+    FOS\HttpCacheBundle\FOSHttpCacheBundle::class => ['all' => true],
+    Sulu\Bundle\AdminBundle\SuluAdminBundle::class => ['all' => true],
+    Sulu\Bundle\SearchBundle\SuluSearchBundle::class => ['all' => true],
+    Sulu\Bundle\PersistenceBundle\SuluPersistenceBundle::class => ['all' => true],
+    Sulu\Bundle\ContactBundle\SuluContactBundle::class => ['all' => true],
+    Sulu\Bundle\MediaBundle\SuluMediaBundle::class => ['all' => true],
+    Sulu\Bundle\SecurityBundle\SuluSecurityBundle::class => ['all' => true],
+    Sulu\Bundle\CategoryBundle\SuluCategoryBundle::class => ['all' => true],
+    Sulu\Bundle\SnippetBundle\SuluSnippetBundle::class => ['all' => true],
+    Sulu\Bundle\PageBundle\SuluPageBundle::class => ['all' => true],
+    Sulu\Bundle\TagBundle\SuluTagBundle::class => ['all' => true],
+    Sulu\Bundle\WebsiteBundle\SuluWebsiteBundle::class => ['all' => true],
+    Sulu\Bundle\LocationBundle\SuluLocationBundle::class => ['all' => true],
+    Sulu\Bundle\HttpCacheBundle\SuluHttpCacheBundle::class => ['all' => true],
+    Sulu\Bundle\DocumentManagerBundle\SuluDocumentManagerBundle::class => ['all' => true],
+    Sulu\Bundle\HashBundle\SuluHashBundle::class => ['all' => true],
+    Sulu\Bundle\CustomUrlBundle\SuluCustomUrlBundle::class => ['all' => true],
+    Sulu\Bundle\RouteBundle\SuluRouteBundle::class => ['all' => true],
+    Sulu\Bundle\MarkupBundle\SuluMarkupBundle::class => ['all' => true],
+    PHPCR\PhpcrMigrationsBundle\PhpcrMigrationsBundle::class => ['all' => true],
+    Massive\Bundle\BuildBundle\MassiveBuildBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Sulu\Bundle\TestBundle\SuluTestBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Sulu\Bundle\PreviewBundle\SuluPreviewBundle::class => ['all' => true],
+    FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true],
+    Symfony\Cmf\Bundle\RoutingBundle\CmfRoutingBundle::class => ['all' => true, 'website' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Sulu\Bundle\ActivityBundle\SuluActivityBundle::class => ['all' => true],
+    Sulu\Bundle\TrashBundle\SuluTrashBundle::class => ['all' => true],
+    Sulu\Bundle\ReferenceBundle\SuluReferenceBundle::class => ['all' => true],
+    Scheb\TwoFactorBundle\SchebTwoFactorBundle::class => ['all' => true],
+    ONGR\ElasticsearchBundle\ONGRElasticsearchBundle::class => ['all' => true],
+    Sulu\Bundle\ArticleBundle\SuluArticleBundle::class => ['all' => true],
 ];
