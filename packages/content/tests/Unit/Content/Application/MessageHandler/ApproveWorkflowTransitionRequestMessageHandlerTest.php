1b34da8344f1f80dbbb314fdbd11abac7d378be6
Prokyonn <daniel@sulu.io>
2026-07-23 15:00:02 +0200

    Add WorkflowTransitionRequest message handlers

---
+++ packages/content/tests/Unit/Content/Application/MessageHandler/ApproveWorkflowTransitionRequestMessageHandlerTest.php
@@ -0,0 +1,97 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) Sulu GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Content\Tests\Unit\Content\Application\MessageHandler;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
+use Prophecy\PhpUnit\ProphecyTrait;
+use Sulu\Component\Security\Authentication\UserInterface;
+use Sulu\Content\Application\Message\ApproveWorkflowTransitionRequestMessage;
+use Sulu\Content\Application\MessageHandler\ApproveWorkflowTransitionRequestMessageHandler;
+use Sulu\Content\Application\RequestWorkflow\RequestWorkflowEvaluatorInterface;
+use Sulu\Content\Domain\Model\WorkflowTransitionRequest\WorkflowTransitionRequest;
+use Sulu\Content\Domain\Repository\WorkflowTransitionRequestRepositoryInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+#[CoversClass(ApproveWorkflowTransitionRequestMessageHandler::class)]
+class ApproveWorkflowTransitionRequestMessageHandlerTest extends TestCase
+{
+    use ProphecyTrait;
+
+    public function testInvokeAddsApprovalAndRefreshesStatus(): void
+    {
+        $requestId = 'some-uuid';
+        $comment = 'Looks good';
+
+        $user = $this->prophesize(UserInterface::class)->reveal();
+        $creator = $this->prophesize(UserInterface::class)->reveal();
+
+        $request = new WorkflowTransitionRequest('pages', 'res-1', 'en');
+        $request->setCreator($creator);
+
+        $repository = $this->prophesize(WorkflowTransitionRequestRepositoryInterface::class);
+        $repository->getOneBy(['id' => $requestId])->willReturn($request);
+
+        $token = $this->prophesize(TokenInterface::class);
+        $token->getUser()->willReturn($user);
+
+        $tokenStorage = $this->prophesize(TokenStorageInterface::class);
+        $tokenStorage->getToken()->willReturn($token->reveal());
+
+        $evaluator = $this->prophesize(RequestWorkflowEvaluatorInterface::class);
+        $evaluator->refreshStatus($request, null)->shouldBeCalledOnce();
+
+        $handler = new ApproveWorkflowTransitionRequestMessageHandler(
+            $repository->reveal(),
+            $tokenStorage->reveal(),
+            $evaluator->reveal(),
+        );
+
+        $message = new ApproveWorkflowTransitionRequestMessage($requestId, $comment);
+        $result = $handler($message);
+
+        $this->assertSame($request, $result);
+        $this->assertCount(1, $result->getReviewers());
+        $this->assertSame($comment, $result->getReviewers()[0]->getComment());
+    }
+
+    public function testInvokeThrowsWhenNoValidUser(): void
+    {
+        $requestId = 'some-uuid';
+
+        $creator = $this->prophesize(UserInterface::class)->reveal();
+        $request = new WorkflowTransitionRequest('pages', 'res-1', 'en');
+        $request->setCreator($creator);
+
+        $repository = $this->prophesize(WorkflowTransitionRequestRepositoryInterface::class);
+        $repository->getOneBy(['id' => $requestId])->willReturn($request);
+
+        $tokenStorage = $this->prophesize(TokenStorageInterface::class);
+        $tokenStorage->getToken()->willReturn(null);
+
+        $evaluator = $this->prophesize(RequestWorkflowEvaluatorInterface::class);
+        $evaluator->refreshStatus(Argument::any(), Argument::any())->shouldNotBeCalled();
+
+        $handler = new ApproveWorkflowTransitionRequestMessageHandler(
+            $repository->reveal(),
+            $tokenStorage->reveal(),
+            $evaluator->reveal(),
+        );
+
+        $this->expectException(\RuntimeException::class);
+        $handler(new ApproveWorkflowTransitionRequestMessage($requestId));
+    }
+}
